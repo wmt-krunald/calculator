ee044baefe3cae87832fb34d38925aa1d3c51b5a
wmt-krunald <wmt-krunald@users.noreply.github.com>
2022-03-21 06:42:01 +0000

    deploy: d8d0274c4ff098cf5fac79b1b15b1124c251dc2d

---
+++ precache-manifest.f586dcbb9c805bbc47cf7ad1e05b7e28.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5abc7be1c8b867761120b072899fb1dd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "58e3861956b6dcf89c5b",
+    "url": "/static/css/2.f87fbcaa.chunk.css"
+  },
+  {
+    "revision": "1efe34ac206d8bebc9c0",
+    "url": "/static/css/main.6fdcdd19.chunk.css"
+  },
+  {
+    "revision": "58e3861956b6dcf89c5b",
+    "url": "/static/js/2.9067ee69.chunk.js"
+  },
+  {
+    "revision": "1efe34ac206d8bebc9c0",
+    "url": "/static/js/main.027dc7db.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
